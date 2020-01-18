636ee730fb5c9bf1754ebbd6475f3bbd27c08ffe
Justin <jk213509@users.noreply.github.com>
2020-01-18 16:00:28 -0600

    wheel-tracker: initial class commit (lacks usage)

---
+++ src/main/java/frc/robot/custom/WheelTracker.java
@@ -0,0 +1,31 @@
+package frc.robot.custom;
+
+import edu.wpi.first.wpilibj.util.Color;
+
+public class WheelTracker {
+    private int num_new_colors = 0;
+    private int num_new_colors_threshold;
+    private Color last_color;
+    private double radians_turned = 0.0;
+
+    public WheelTracker(Color first_color, int threshold) {
+        assert threshold >= 1;
+        this.last_color = first_color;
+        this.num_new_colors_threshold = threshold;
+    }
+
+    public void setNewColor(Color new_color) {
+        if(new_color == this.last_color) {
+            this.num_new_colors++;
+        }
+        if(this.num_new_colors >= this.num_new_colors_threshold) {
+            this.radians_turned += Math.PI / 4.0;
+            this.last_color = new_color;
+            this.num_new_colors = 0;
+        }
+    }
+
+    public double getNumRotations() {
+        return this.radians_turned / (2.0*Math.PI);
+    }
+}
