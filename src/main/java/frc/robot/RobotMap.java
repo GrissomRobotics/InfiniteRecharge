7f68ccacf3b1e0d18d9f865983dfa05d08a527c9
Justin <jk213509@users.noreply.github.com>
2020-01-30 18:47:44 -0600

    cameras: untested method to stream selectable cameras (one at a time)

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -32,6 +32,15 @@ import edu.wpi.first.wpilibj.SerialPort.Parity;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.SerialPort.StopBits;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+
 /**
  * Add your docs here.
  */
@@ -57,20 +66,73 @@ public class RobotMap {
     public static final double SPINNER_WHEEL_SPEED = 0.5;
     public static final double WINCH_SPEED = 0.5;
 
+    // camera
+    Thread m_visionThread;
+    int camera_selection = 0;
+
     public RobotMap() {
 
         // spinner and color sensor
-        //i2cPort = I2C.Port.kOnboard;
-        //m_colorSensor = new ColorSensorV3(i2cPort);
+        // i2cPort = I2C.Port.kOnboard;
+        // m_colorSensor = new ColorSensorV3(i2cPort);
 
         // sensor
-        //ultraSerial = new SerialPort(9600, Port.kOnboard, 8, Parity.kNone, StopBits.kOne);
-        //ultraSerial.reset();
+        // ultraSerial = new SerialPort(9600, Port.kOnboard, 8, Parity.kNone,
+        // StopBits.kOne);
+        // ultraSerial.reset();
 
-        //gyro = new PigeonIMU(0);
-        //ultra = new UltrasonicSensor(ultraSerial);
+        // gyro = new PigeonIMU(0);
+        // ultra = new UltrasonicSensor(ultraSerial);
 
         driveTrain.setDefaultCommand(new DriveWithJoystick(driveTrain, oi));
+
+        m_visionThread = new Thread(() -> {
+            // Get the UsbCamera from CameraServer
+            UsbCamera camera0 = CameraServer.getInstance().startAutomaticCapture(0);
+            UsbCamera camera1 = CameraServer.getInstance().startAutomaticCapture(1);
+            // Set the resolution
+            camera0.setResolution(640, 480);
+            camera1.setResolution(640, 480);
+
+            // Get a CvSink. This will capture Mats from the camera
+            CvSink cvSink0 = CameraServer.getInstance().getVideo(camera0);
+            CvSink cvSink1 = CameraServer.getInstance().getVideo(camera1);
+            // Setup a CvSource. This will send images back to the Dashboard
+            CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
+
+            // Mats are very memory expensive. Lets reuse this Mat.
+            Mat mat = new Mat();
+
+            // This cannot be 'true'. The program will never exit if it is. This
+            // lets the robot stop this thread when restarting robot code or
+            // deploying.
+            while (!Thread.interrupted()) {
+                // Tell the CvSink to grab a frame from the camera and put it
+                // in the source mat. If there is an error notify the output.
+                if (camera_selection == 0) {
+                    if (cvSink0.grabFrame(mat) == 0) {
+                        // Send the output the error.
+                        outputStream.notifyError(cvSink0.getError());
+                        // skip the rest of the current iteration
+                        continue;
+                    }
+                } else {
+                    if (cvSink1.grabFrame(mat) == 0) {
+                        // Send the output the error.
+                        outputStream.notifyError(cvSink1.getError());
+                        // skip the rest of the current iteration
+                        continue;
+                    }
+                }
+
+                // Put a rectangle on the image
+                Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400), new Scalar(255, 255, 255), 5);
+                // Give the output stream a new image to display
+                outputStream.putFrame(mat);
+            }
+        });
+        m_visionThread.setDaemon(true);
+        m_visionThread.start();
 
     }
 
